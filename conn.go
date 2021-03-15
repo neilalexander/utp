94aa85fe04369d9569f99da391b24c42a1c0b8ad
Neil Alexander <neilalexander@users.noreply.github.com>
2021-03-15 13:30:37 +0000

    Don't use sendBufferPool, it's buggy

--- conn.go
+++ conn.go
@@ -125,7 +125,8 @@ func (c *Conn) makePacket(_type st, connID, seqNr uint16, payload []byte) (p []b
 			Bytes: selAck.Bytes,
 		})
 	}
-	p = sendBufferPool.Get().([]byte)[:0:minMTU]
+	//p = sendBufferPool.Get().([]byte)[:0:minMTU]
+	p = make([]byte, 0, minMTU)
 	n := h.Marshal(p)
 	p = p[:n]
 	// Extension headers are currently fixed in size.
@@ -140,7 +141,7 @@ func (c *Conn) makePacket(_type st, connID, seqNr uint16, payload []byte) (p []b
 func (c *Conn) send(_type st, connID uint16, payload []byte, seqNr uint16) (err error) {
 	p := c.makePacket(_type, connID, seqNr, payload)
 	n1, err := c.socket.writeTo(p, c.remoteSocketAddr)
-	sendBufferPool.Put(p[:0:minMTU])
+	//sendBufferPool.Put(p[:0:minMTU])
 	if err != nil {
 		return
 	}
